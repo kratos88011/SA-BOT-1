0cc94c2748b00e3bb0785fff403b13c76af69f52
kratos88011 <161939712+kratos88011@users.noreply.github.com>
2024-03-02 12:46:05 +0100

    Update SLY_Lab_Assistant.user.js

--- SLY_Lab_Assistant.user.js
+++ SLY_Lab_Assistant.user.js
@@ -6,9 +6,9 @@
 // @author       SLY w/ Contributions by SkyLove512, anthonyra, niofox
 // @match        https://*.labs.staratlas.com/
 // @require      https://unpkg.com/@solana/web3.js@latest/lib/index.iife.min.js
-// @require      https://raw.githubusercontent.com/ImGroovin/SAGE-Lab-Assistant/main/anchor-browserified.js
-// @require      https://raw.githubusercontent.com/ImGroovin/SAGE-Lab-Assistant/main/buffer-browserified.js
-// @require      https://raw.githubusercontent.com/ImGroovin/SAGE-Lab-Assistant/main/bs58-browserified.js
+// @require      https://raw.githubusercontent.com/kratos88011/SA-BOT-1/main/anchor-browserified.js
+// @require      https://raw.githubusercontent.com/kratos88011/SA-BOT-1/main/buffer-browserified.js
+// @require      https://raw.githubusercontent.com/kratos88011/SA-BOT-1/main/bs58-browserified.js
 // @icon         https://www.google.com/s2/favicons?sz=64&domain=staratlas.com
 // @grant        GM_setValue
 // @grant        GM_getValue
@@ -20,7 +20,7 @@
 
     let enableAssistant = false;
     let initComplete = false;
-    let rpcEndpoints = ['https://rpc.hellomoon.io/cfd5910f-fb7d-4489-9b32-f97193eceefd','https://solana-api.syndica.io/access-token/WPoEqWQ2auQQY1zHRNGJyRBkvfOLqw58FqYucdYtmy8q9Z84MBWwqtfVf8jKhcFh/rpc'];
+    let rpcEndpoints = ['https://solana-mainnet.g.alchemy.com/v2/uhnRGha-4iwruFJ1bjOp15bGzWT7O3sO','https://rpc.hellomoon.io/6ba0fb87-dc78-4a60-b766-ee064e63b2a7'];
     const priorityFee = 1; // Priority Fee added to each transaction in Lamports. Set to 0 (zero) to disable priority fees. 1 Lamport = 0.000000001 SOL
 
     const connectionProxy = {
@@ -755,8 +755,8 @@
             let txHash = response.txHash;
             let confirmation = response.confirmation;
             let txResult = await solanaConnection.getTransaction(txHash, {commitment: 'confirmed', preflightCommitment: 'confirmed', maxSupportedTransactionVersion: 1});
-            //let priorityHistory = await solanaConnection.getRecentPrioritizationFees();
-            //console.log('priorityHistory: ', priorityHistory);
+            /*let priorityHistory = await solanaConnection.getRecentPrioritizationFees();
+            //console.log('priorityHistory: ', priorityHistory);*/
             if (confirmation.name == 'TransactionExpiredBlockheightExceededError' && !txResult) {
                 console.log('-----RETRY-----');
                 txResult = await txSignAndSend(ix);
